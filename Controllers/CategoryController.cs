c08fead4b8826201f55a3d75e5ac27355617c80d
Brayan Landa <landakkpp@gmail.com>
2026-09-14 21:51:18 -0500

    feat: implement Create, Update, and Delete endpoints for category management

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,3 +1,4 @@
+using ApiEcommerce.Models;
 using ApiEcommerce.Models.Dtos;
 using ApiEcommerce.Repository;
 using AutoMapper;
@@ -39,9 +40,87 @@ public class CategoryController : ControllerBase
     public IActionResult GetCategory(int id)
     {
         var category = _categoryRepository.GetCategory(id);
-        if(category is null) NotFound("Category not found");
+        if (category is null) NotFound("Category not found");
         var categoryDto = _mapper.Map<CategoryDto>(category);
-        
+
         return Ok(categoryDto);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult CreateCategory([FromBody] CreateCategoryDto createCategoryDto)
+    {
+        if (createCategoryDto == null) return BadRequest(ModelState);
+
+        if (_categoryRepository.CategoryExists(createCategoryDto.Name))
+        {
+            ModelState.AddModelError("CustomError", "Category already exists");
+            return BadRequest(ModelState);
+        }
+
+        var category = _mapper.Map<Category>(createCategoryDto);
+        if (!_categoryRepository.CreateCategory(category))
+        {
+            ModelState.AddModelError("CustomError", $"Something went wrong {category.Name}");
+            return StatusCode(500, ModelState);
+        }
+        return CreatedAtRoute("GetCategory", new { id = category.Id }, category);
+    }
+
+    [HttpPatch("{id:int}", Name = "UpdateCategory")]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult UpdateCategory(int id, [FromBody] CreateCategoryDto updateCategoryDto)
+    {
+        if (!_categoryRepository.CategoryExists(id))
+            return NotFound($"Category not found {id}");
+
+        if (updateCategoryDto == null) return BadRequest(ModelState);
+
+        if (_categoryRepository.CategoryExists(updateCategoryDto.Name))
+        {
+            ModelState.AddModelError("CustomError", "Category already exists");
+            return BadRequest(ModelState);
+        }
+
+        var category = _mapper.Map<Category>(updateCategoryDto);
+        category.Id = id;
+        if (!_categoryRepository.UpdateCategory(category))
+        {
+            ModelState.AddModelError("CustomError", $"Something went wrong {category.Name}");
+            return StatusCode(500, ModelState);
+        }
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}", Name = "DeleteCategory")]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult DeleteCategory(int id)
+    {
+        if (!_categoryRepository.CategoryExists(id))
+            return NotFound($"Category not found {id}");
+
+        var category = _categoryRepository.GetCategory(id);
+
+        if (category == null)
+            return NotFound($"Category not found {id}");
+
+        if (!_categoryRepository.DeleteCategory(category))
+        {
+            ModelState.AddModelError("CustomError", $"Something went wrong {category.Name}");
+            return StatusCode(500, ModelState);
+        }
+        return NoContent();
     }
 }

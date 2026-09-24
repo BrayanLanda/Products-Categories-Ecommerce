d358b2b816350256439eabeff06e1f9017a0d427
Brayan Landa <landakkpp@gmail.com>
2026-09-23 20:48:47 -0500

    feat: add User DTOs and AutoMapper profile for user management

--- Models/Dtos/UserDto.cs
+++ Models/Dtos/UserDto.cs
@@ -1,13 +1,10 @@
-using System.ComponentModel.DataAnnotations;
+namespace ApiEcommerce.Models.Dtos;
 
-namespace ApiEcommerce.Models;
-
-public class User
+public class UserDto
 {
-    [Key]
     public int Id { get; set; }
     public string? Name { get; set; }
-    public string? UserName { get; set; }
+    public string? Username { get; set; }
     public string? Password { get; set; }
     public string? Role { get; set; }
 }
---
+++ Models/Dtos/UserLoginDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiEcommerce.Models.Dtos;
+
+public class UserLoginDto
+{
+    [Required(ErrorMessage = "The username is required")]
+    public string? Username { get; set; }
+
+    [Required(ErrorMessage = "The password is required")]
+    public string? Password { get; set; }
+}
